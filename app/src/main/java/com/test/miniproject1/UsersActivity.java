a566999cd7a032e47be15eaa7e1ff5fe9faa01bd
Dakir <dakirabdellah3@gmail.com>
2024-05-08 01:30:38 +0100

    read from RAW folder

--- app/src/main/java/com/test/miniproject1/UsersActivity.java
+++ app/src/main/java/com/test/miniproject1/UsersActivity.java
@@ -1,24 +1,44 @@
 package com.test.miniproject1;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class UsersActivity extends AppCompatActivity {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+public class UsersActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnUsersActLoadUsers, btnUsersActQuit;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_users);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        btnUsersActLoadUsers = findViewById(R.id.btnUsersActLoadUsers);
+        btnUsersActQuit = findViewById(R.id.btnUsersActQuit);
+
+        btnUsersActLoadUsers.setOnClickListener(this);
+        btnUsersActQuit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnUsersActLoadUsers) {
+            InputStream inputStream = getResources().openRawResource(R.raw.users);
+            try {
+                Toast.makeText(this, Character.toString((char) inputStream.read()), Toast.LENGTH_SHORT).show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (v.getId() == R.id.btnUsersActQuit) {
+            finish();
+        }
     }
 }
